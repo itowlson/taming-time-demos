96993dfa84fbd81707c035d08fbb152c8e353e32
Ivan Towlson <ivan@greenbutton.com>
2013-09-06 12:46:14 +1200

    Observables from events - file system

---
+++ RxGui/FileSystem.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RxGui
+{
+    public static class FileSystem
+    {
+        public static IObservable<FileSystemEventArgs> WhatsHappeningIn(string path)
+        {
+            FileSystemWatcher watcher = new FileSystemWatcher();
+            watcher.Path = path;
+
+            var changes = Observable.FromEvent<FileSystemEventHandler, FileSystemEventArgs>(a => new FileSystemEventHandler((o, e) => a(e)), h => watcher.Changed += h, h => watcher.Changed -= h);
+            var creations = Observable.FromEvent<FileSystemEventHandler, FileSystemEventArgs>(a => new FileSystemEventHandler((o, e) => a(e)), h => watcher.Created += h, h => watcher.Created -= h);
+            var deletions = Observable.FromEvent<FileSystemEventHandler, FileSystemEventArgs>(a => new FileSystemEventHandler((o, e) => a(e)), h => watcher.Deleted += h, h => watcher.Deleted -= h);
+            var renamings = Observable.FromEvent<RenamedEventHandler, RenamedEventArgs>(a => new RenamedEventHandler((o, e) => a(e)), h => watcher.Renamed += h, h => watcher.Renamed -= h);
+            var events = changes.Merge(creations).Merge(deletions).Merge(renamings);
+
+            watcher.EnableRaisingEvents = true;
+
+            return events;
+        }
+
+    }
+}
---
+++ RxGui/FileSystemDemo.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Reactive.Concurrency;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace RxGui
+{
+    /// <summary>
+    /// Interaction logic for FileSystemDemo.xaml
+    /// </summary>
+    public partial class FileSystemDemo : Window
+    {
+        private readonly FileSystemDemoViewModel _viewModel = new FileSystemDemoViewModel();
+
+        public FileSystemDemo()
+        {
+            InitializeComponent();
+
+            DataContext = _viewModel;
+
+            FileSystem.WhatsHappeningIn("d:\\temp")
+                      .Select(e => e.ChangeType + ": " + e.Name)
+                      .Subscribe(s => { _viewModel.LastChange = s; });
+
+            //FileSystem.WhatsHappeningIn("d:\\temp")
+            //          .Select(e => e.ChangeType + ": " + e.Name)
+            //          .Subscribe(s => _viewModel.Events.Add(s));
+
+            //FileSystem.WhatsHappeningIn("d:\\temp")
+            //          .Select(e => e.ChangeType + ": " + e.Name)
+            //          .Subscribe(s => fileSystemEventList.Items.Add(s));
+        }
+    }
+
+    public class FileSystemDemoViewModel : ViewModelBase
+    {
+        private readonly ObservableCollection<string> _events = new ObservableCollection<string>();
+
+        public ICollection<string> Events
+        {
+            get { return _events; }
+        }
+
+        private string _lastChange;
+
+        public string LastChange
+        {
+            get { return _lastChange; }
+            set { Set(ref _lastChange, value, "LastChange"); }
+        }
+    }
+}
